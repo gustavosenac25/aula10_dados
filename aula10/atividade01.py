f677c37c4a4ed32a27e5e3473c6e9beb3e77ab4d
gustavosenac25 <gustavo.ocosta@aluno.senac.rio>
2025-12-16 21:39:43 -0300

    Pos exercicío

--- aula10/atividade01.py
+++ aula10/atividade01.py
@@ -1,2 +1,18 @@
 # Atividade  01
 # VsCode - GitHub
+
+def soma_numeros(a , b):
+    soma = a + b
+    return soma
+
+
+for num in range(3):
+    try:
+        numeroa = float(input('Informe o primeiro número: '))
+        numerob = float(input('Informe o segundo número: '))
+        resultado = soma_numeros(numeroa, numerob)
+        print(f'A soma de {numeroa} e {numerob} é: {resultado}')
+    except ValueError:
+        print('Digite apenas números')
+    except TypeError:
+        print('Digite apenas números')
