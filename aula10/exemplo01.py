64574e2b8c54fa8f3d74e68532ccd7949f2073bc
gustavosenac25 <gustavo.ocosta@aluno.senac.rio>
2025-12-16 21:03:00 -0300

    Antes dos exercícios

---
+++ aula10/exemplo01.py
@@ -0,0 +1,58 @@
+# def calcula_imc(p,a):
+#     imc = p / (a ** 2)
+#     return imc
+
+# peso = float(input('Informe o peso: '))
+# altura = float(input('Infome a altura: '))
+
+# imc_calculado = calcula_imc(peso , altura)
+# print(imc_calculado)
+
+# if imc_calculado < 17:
+#     print('Muito abaixo do peso')
+# elif imc_calculado <= 18.4:
+#     print('Abaixo do peso')
+# elif imc_calculado <= 29.9:
+#     print('Peso normal')
+# elif imc_calculado <= 34.9:
+#     print('Obesidade grau I')
+# elif imc_calculado <= 40:
+#     print('Obesidade grau II')
+# else:
+#     print('Obesidade grau III')
+
+
+def calcula_atingimento(v , m):
+    r = v / m * 100
+    return r
+
+# for nun in range(1,4):
+#     try:
+#         venda = float(input('Informe o valor da venda: '))
+#         meta = float(input ('Informe a meta do vendor: '))
+#         resultado = calcula_atingimento(venda , meta)
+#         print(f'Resultado: {resultado}%')
+#     except ValueError:
+#         print('\nErro! Digite apenas números')
+#     except ZeroDivisionError:
+#         print('\nErro! Digite um valor diferente de zero')
+
+#ou
+
+for nun in range(1,4):
+    try:
+        venda = float(input('Informe o valor da venda: '))
+        meta = float(input ('Informe a meta do vendor: '))
+        resultado = calcula_atingimento(venda , meta)
+    except ValueError:
+        print('\nErro! Digite apenas números')
+    except ZeroDivisionError:
+        print('\nErro! Digite um valor diferente de zero')
+        exit()
+    except KeyboardInterrupt:
+        print('\nOperação finalizada pelo usuário')
+    else:
+        print(f'Resultado: {resultado}%')
+    finally:
+        print('Operação finalizada')
+    
---
+++ aula10/exemplo02.py
@@ -0,0 +1,24 @@
+lst_vendas = [] #Sem nada dentro da chave significa que é infinita
+resposta = 's'
+venda = 1 #para que comece no item 1
+tentativa = 1
+
+#enquanto as respostas do usúario for sim, continua a repetição
+
+while resposta != 'n': #enquanto a resposta for diferente de n, ou seja, for sim para a pergunta la em baixo: Quer continuar?
+    try:
+        valor = float(input(f'Informe o valor da {venda}ª venda: '))
+    except ValueError:
+        print('Informe apenas números')
+    except KeyboardInterrupt:
+        print('Operação encerrada pelo usuário')
+        break
+    else:
+        lst_vendas.append(valor) #adicionar o valor na lista
+        resposta = input('Quer continuar? [s/n]') [0].lower() #caso o unuario digite SIM ou NAO, esse codigo usa somente a primeira letra e torna tudo em minisculo
+        venda += 1 #venda passa a signicar 2... depois 3... depois 4...
+    finally:
+        print(f'Tentativa {tentativa} \n')
+        tentativa += 1
+
+print(f'Vendas registradas: {lst_vendas}')
